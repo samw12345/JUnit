8911e24e7bbc6835dcf4d227b9eab8216e26163f
samw12345 <samw.london2019a@gmail.com>
2020-01-07 15:14:27 +0000

    push

---
+++ src/main/java/exercises/Angle.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class Angle {
+    private int degrees ;
+    public int getDegrees()
+    {
+        return degrees ;
+    }
+    private void setDegrees( int degrees )
+    {
+        this.degrees = degrees ;
+    }
+    public Angle(int degrees){
+        setDegrees(degrees);
+    }
+
+
+    }
---
+++ src/test/java/exercises/AngleTest.java
@@ -0,0 +1,17 @@
+package exercises;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AngleTest
+{
+    @BeforeEach
+    public void setUp() throws Exception
+    {
+    }
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+}
